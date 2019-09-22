071bbd0e5d435d745665534ec8fa7a48c9046cce
Assaf Eli <ASSAFEL@amdocs.com>
2019-09-22 17:34:45 +0300

    Returning Twilio token as JWT

---
+++ src/components/startForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react"
+import "./style.css"
+import Axios from "axios"
+
+const StartForm = () => {
+  const [name, setName] = useState("")
+  const [room, setRoom] = useState("")
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    const result = await Axios({
+      method: "POST",
+      url: "https://straw-serval-7941.twil.io/createToken",
+      data: { identity: name },
+      //withCredentials: true,
+    })
+    console.log(result)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="user-name">
+        Display Name <br />
+        <input
+          type="text"
+          name="user-name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          id="user-name"
+        />
+      </label>
+      <label htmlFor="user-room">
+        Room to join <br />
+        <input
+          type="text"
+          name="user-room"
+          value={room}
+          onChange={e => setRoom(e.target.value)}
+          id="user-room"
+        />
+      </label>
+      <button type="submit">Join Room</button>
+    </form>
+  )
+}
+
+export default StartForm
