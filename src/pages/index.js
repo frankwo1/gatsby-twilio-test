4e5e90d68bd54a7245b200402445ad1d1280e086
Assaf Eli <ASSAFEL@amdocs.com>
2019-09-23 14:04:43 +0300

    add remote participants

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,27 +8,35 @@ const Video = ({ token }) => {
   const localVidRef = useRef(null)
   const RemoteVidRef = useRef(null)
 
+  const addParticipant = participant => {
+    //Attach video to all remote participants]
+    console.log("Participant joined room")
+    console.log(participant.identity)
+    participant.tracks.forEach(publication => {
+      if (publication.isSubscribed) {
+        const track = publication.track
+
+        RemoteVidRef.current.appendChild(track.attach())
+      }
+    })
+  }
+
   useEffect(() => {
     TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
       room => {
+        //Attach video to local user
         TwilioVideo.createLocalVideoTrack().then(track => {
           localVidRef.current.appendChild(track.attach())
+          console.log("successfully joined room")
+          console.log(room)
         })
-        room.participants.forEach(participant => {
-          participant.tracks.forEach(publication => {
-            if (publication.isSubscribed) {
-              const track = publication.track
 
-              RemoteVidRef.current.appendChild(track.attach())
-            }
-          })
-        })
-        console.log("successfully joined room")
-        console.log(room)
+        room.on("participantConnected", addParticipant)
+
+        //Attach video to all remote participants
+        room.participants.forEach(addParticipant)
       }
     )
-
-    return () => {}
   }, [token])
 
   return (
